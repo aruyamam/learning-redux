f87096cc2a469e3f35f601e1db842a66aa0569a4
aruyamam <aruyamam@gmail.com>
2018-10-13 14:31:18 +0900

    Implementing container components

--- src/containers/ConnectedPostList.jsx
+++ src/containers/ConnectedPostList.jsx
@@ -1,7 +1,23 @@
 import { connect } from 'react-redux';
 import PostList from '../components/PostList.jsx';
 
-const mapStateToProps = (state, props) => ({ posts: state.posts });
+const mapStateToProps = (state, props) => {
+   const filteredPosts = state.filter
+      ? state.posts.filter(post => post.category === state.filter)
+      : state.posts;
+
+   return {
+      posts: filteredPosts.msp((post) => {
+         const { user, ...rest } = post;
+         const userObj = state.users.find(({ username }) => user === username);
+         return {
+            user: userObj,
+            ...rest,
+         };
+      }),
+   };
+};
+
 const ConnectedPostList = connect(mapStateToProps)(PostList);
 
 export default ConnectedPostList;
